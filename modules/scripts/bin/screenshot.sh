32117384a8aff2f0c79210f496f03a4719224ba4
viato115 <nschmidt618@gmail.com>
2024-05-19 21:13:42 +0200

    Added screenshot as bin rather than .sh

---
+++ modules/scripts/bin/screenshot.sh
@@ -0,0 +1,23 @@
+{
+  config,
+  pkgs,
+  lib,
+  inputs,
+  ...
+}: {
+  home.file.".local/bin/screenshot" = {
+    executable = true;
+    text = ''
+      #!/bin/sh
+      NOW=$(date +%s)
+      grim -g "$(slurp )" -t jpeg $HOME/Pictures/Screenshot-$NOW.jpg
+      feh $HOME/Pictures/Screenshot-$NOW.jpg
+    '';
+  };
+}
+
+
+
+
+
+
