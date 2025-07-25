a91a78fa9bb45cf0ecc9cf1e5c193ecc1219b6c9
viato115 <nschmidt618@gmail.com>
2025-07-25 23:21:30 +0200

    Added some options to the nvf module, still not complete yet.

--- modules/tui/programs/nvf/nvf.nix
+++ modules/tui/programs/nvf/nvf.nix
@@ -1,24 +1,55 @@
-{ pgks, lib, ... }:
+{ pkgs, lib, ... }:
+let telescopeFzf = pkgs.vimPlugins.telescope-fzf-native-nvim;
+in
 {
   programs.nvf = {
-    enable = true;
-    settings = {
-      vim = {
-        theme.enable = true;
-        theme.name = "tokyonight";
-        theme.style = "dark";
-        lsp = {
-          enable = true;
-        };
 
-        languages = {
-          enableTreeSitter = true;
-          nix.enable = true;
-          rust.enable = true;
-          python.enable = true;
-          lua.enable = true;
-          markdown.enable = true;
+    # general settings 
+    enable = true;
+    settings.vim = {
+      theme.enable = true;
+      theme.name = "tokyonight";
+      theme.style = "night";
+      viAlias = true;
+      vimAlias = true;
+      lsp = {
+        enable = true;
+      };
+      
+
+      # plugins
+      dashboard.alpha.enable = true;
+
+      telescope = {
+        enable = true;
+        extensions = [
+          {
+            name = "fzf";
+            packages = [ telescopeFzf ];
+            setup = { fzf = { fuzzy = true; }; };
+          }
+        ];
+      };
+      
+      filetree.nvimTree = {
+        enable = true;
+        mappings = {
+          findFile = "<leader>ff";
+          toggle = "<leader>e";
         };
+      };
+
+
+
+
+
+      languages = {
+        enableTreesitter = true;
+        nix.enable = true;
+        rust.enable = true;
+        python.enable = true;
+        lua.enable = true;
+        markdown.enable = true;
       };
     };
   };
