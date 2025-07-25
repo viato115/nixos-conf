3d6e85f719088e4588424a792572c7da53feb486
viato115 <nschmidt618@gmail.com>
2025-07-25 23:19:26 +0200

    Modified the nh module a bit, not working yet.

--- modules/tui/programs/nh/nh.nix
+++ modules/tui/programs/nh/nh.nix
@@ -1,7 +1,13 @@
-{ config, pkgs, lib, ... }:
+{ pkgs, ... }:
 {
+  environment.variables.FLAKE = "$HOME/.config/nixos#nixpad";
   programs.nh = {
     enable = true;
-    flake = "/home/nico/.config/nixos";
+    flake = "/home/nico/.config/nixos#nixpad";
+    
+    clean = {
+      enable = false;
+      extraArgs = "--keep-since 7d --keep 3";
+    };
   };
 }
