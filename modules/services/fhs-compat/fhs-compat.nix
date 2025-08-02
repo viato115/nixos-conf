d8d05ac9b30a29d5f29a0cb3f589d2fcc0671e71
viato115 <nschmidt618@gmail.com>
2025-08-02 17:46:32 +0200

    Expanded the fhs-compat module.

--- modules/services/fhs-compat/fhs-compat.nix
+++ modules/services/fhs-compat/fhs-compat.nix
@@ -2,34 +2,67 @@
   
 ### Special thanks to github:Mic92 for this
 
+  services.envfs.enable = lib.mkDefault true;
+
   programs.nix-ld = {
     enable = true;
     libraries = with pkgs; [
-      stdenv.cc.cc
-      zlib
-      fuse3
-      openssl
-      curl
-      dbus
+      acl
       attr
       bzip2
+      dbus
+      expat
+      fontconfig
+      freetype
+      fuse3
+      icu
       libnotify
+      libsodium
+      libssh
+      libunwind
       libusb1
       libuuid
+      nspr
+      nss
+      stdenv.cc.cc
       util-linux
-      zstd
-    ]
+      zlib
+      zstd 
+     ]
     ++ lib.optionals (config.hardware.graphics.enable) [
       pipewire
       cups
+      libxkbcommon
+      pango
       mesa
+      libdrm
+      libglvnd
       libpulseaudio
-      alsa-lib
+      atk
       cairo
+      alsa-lib
+      at-spi2-atk
+      at-spi2-core
+      gdk-pixbuf
       glib
       gtk3
       libGL
+      libappindicator-gtk3
       vulkan-loader
+      xorg.libX11
+      xorg.libXScrnSaver
+      xorg.libXcomposite
+      xorg.libXcursor
+      xorg.libXdamage
+      xorg.libXext
+      xorg.libXfixes
+      xorg.libXi
+      xorg.libXrandr
+      xorg.libXrender
+      xorg.libXtst
+      xorg.libxcb
+      xorg.libxkbfile
+      xorg.libxshmfence
     ];
   };
 }
