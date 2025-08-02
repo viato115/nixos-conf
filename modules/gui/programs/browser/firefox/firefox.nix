a9fcb5da0daf43ebc90d51d53b2f405150612a3b
viato115 <nschmidt618@gmail.com>
2025-08-02 13:38:56 +0200

    Modified my firefox module. Thanks to github:scientiac for the inspiration!

--- modules/gui/programs/browser/firefox/firefox.nix
+++ modules/gui/programs/browser/firefox/firefox.nix
@@ -1,74 +1,131 @@
-{ 
-  inputs,
-  options,
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
+{ pkgs, inputs, lib, ... }:
+{
+### Shamelessly stole and modified the beautiful Firefox userStyle "scifox" from github:scientiac. Thanks for the upload :)
+
   programs.firefox = {
     enable = true;
-
-
-    profiles.nicolas = {
-      bookmarks = {};
-
-      extensions.packages = with inputs.firefox-addons.packages.${pkgs.system}; [ 
-        ublock-origin 
-        localcdn
-        privacy-badger
-      ];
-
-
-      userChrome = builtins.readFile ./userChrome.css;
-      userContent = builtins.readFile ./userContent.css;
-
-      settings = {
-        "browser.startup.homepage" = "https://viato115.github.io/personalbento/";
-        "browser.search.defaultenginename" = "google";
-        "browser.search.order.1" = "google";
+    package = pkgs.wrapFirefox pkgs.firefox-beta-unwrapped {
+      extraPolicies = {
+        CaptivePortal = false;
+        DisableFirefoxStudies = true;
+        DisablePocket = true;
+        DisableTelemetry = true;
+        DisalbeFirefoxAccounts = false;
+        NoDefaultBookmarks = true;
+        OfferToSaveLogins = false;
+        OfferToSaveLoginsDefault = false;
+        PasswordManagerEnabled = false;
+        FirefoxHome = {
+          Search = true;
+          Pocket = false;
+          Snippets = false;
+          TopSites = false;
+          Highlights = false;
+        };
+        UserMessaging = {
+          ExtensionReccomendations = false;
+          SkipOnboarding = true;
+        };
       };
+    };
 
-      search = {
-        force = true;
-        default = "google";
-        engines = {
-          "Nix Packages" = {
-            urls = [{ template = "https://search.nixos.org/packages";
-              params = [
-                { name = "type"; value = "packages"; }
-                { name = "query"; value = "{searchTerms}"; }
-              ];
-            }];
-            icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-            definedAliases = [ "@np" ];
+    profiles = {
+      default = {
+        id = 0;
+        name = "nix";
+        isDefault = true;
+
+        extensions.packages = with inputs.firefox-addons.packages.${pkgs.system}; [
+          ublock-origin
+          sponsorblock
+          i-dont-care-about-cookies
+          adaptive-tab-bar-colour
+          localcdn
+          privacy-badger
+        ];
+
+
+        userChrome = builtins.readFile ./userChrome.css;
+        userContent = builtins.readFile ./userContent.css;
+
+
+        settings = {
+          "browser.startup.homepage" = "https://viato115.github.io/personalbento/";
+          "browser.search.defaultenginename" = "google";
+          "browser.search.order.1" = "google";
+		      "widget.use-xdg-desktop-portal.file-picker" = 1;
+          "widget.use-xdg-desktop-portal.mime-handler" = 1;
+          "browser.search.suggest.enabled" = false;
+          "browser.search.suggest.enabled.private" = false;
+          "browser.urlbar.suggest.searches" = false;
+          "browser.urlbar.showSearchSuggestionsFirst" = false;
+          "browser.sessionstore.enabled" = true;
+          "browser.sessionstore.resume_from_crash" = true;
+          "browser.sessionstore.resume_session_once" = true;
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+          "browser.tabs.drawInTitlebar" = true;
+          "svg.context-properties.content.enabled" = true;
+          "general.smoothScroll" = true;
+          "uc.tweak.hide-tabs-bar" = true;
+          "uc.tweak.hide-forward-button" = true;
+          "uc.tweak.rounded-corners" = true;
+          "uc.tweak.floating-tabs" = true;
+          "layout.css.color-mix.enabled" = true;
+          "layout.css.light-dark.enabled" = true;
+          "layout.css.has-selector.enabled" = true;
+          "media.ffmpeg.vaapi.enabled" = true;
+          "media.rdd-vpx.enabled" = true;
+          "browser.tabs.tabmanager.enabled" = false;
+          "full-screen-api.ignore-widgets" = false;
+          "browser.urlbar.suggest.engines" = false;
+          "browser.urlbar.suggest.openpage" = false;
+          "browser.urlbar.suggest.bookmark" = false;
+          "browser.urlbar.suggest.addons" = false;
+          "browser.urlbar.suggest.pocket" = false;
+          "browser.urlbar.suggest.topsites" = false;
+          "full-screen-api.warning.timeout" = 0;
+        };
+        search = {
+          force = true;
+          default = "google";
+          engines = {
+            "Nix Packages" = {
+              urls = [{ template = "https://search.nixos.org/packages";
+                params = [
+                  { name = "type"; value = "packages"; }
+                  { name = "query"; value = "{searchTerms}"; }
+                ];
+              }];
+              icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = [ "@np" ];
+            };
+            "Nix Options" = {
+              urls = [{ template = "https://search.nixos.org/options";
+                params = [
+                  { name = "type"; value = "options"; }
+                  { name = "query"; value = "{searchTerms}"; }
+                ];
+              }];
+              icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = [ "@no" ];
+            };
+            "NixOS Wiki" = {
+              urls = [{ template = "https://nixos.wiki/index.php?search={searchTerms}"; }];
+              icon = "https://nixos.wiki/favicon.png";
+              updateInterval = 24 * 60 * 1000;
+              definedAliases = [ "@nw" ];
+            };
+            "Arch Wiki" = {
+              urls = [{ template = "https://wiki.archlinux.org/index.php?search={searchTerms}"; }];
+              definedAliases = [ "@aw" ];
+            };
+            "Home-Manager Options" = {
+              urls = [{ template = "https://home-manager-options.extranix.com/?query={searchTerms}&release=master"; }];
+              definedAliases = [ "@hmo" ];
+              icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            };
+            "google".metaData.alias = "@g";
           };
-          "Nix Options" = {
-            urls = [{ template = "https://search.nixos.org/options";
-              params = [
-                { name = "type"; value = "options"; }
-                { name = "query"; value = "{searchTerms}"; }
-              ];
-            }];
-            icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-            definedAliases = [ "@no" ];
-          };
-          "NixOS Wiki" = {
-            urls = [{ template = "https://nixos.wiki/index.php?search={searchTerms}"; }];
-            icon = "https://nixos.wiki/favicon.png";
-            updateInterval = 24 * 60 * 1000;
-            definedAliases = [ "@nw" ];
-          };
-          "Arch Wiki" = {
-            urls = [{ template = "https://wiki.archlinux.org/index.php?search={searchTerms}"; }];
-            definedAliases = [ "@aw" ];
-          };
-          "Home-Manager Options" = {
-            urls = [{ template = "https://home-manager-options.extranix.com/?query={searchTerms}&release=master"; }];
-            definedAliases = [ "@hmo" ];
-            icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-          };
-          "google".metaData.alias = "@g";
         };
       };
     };
